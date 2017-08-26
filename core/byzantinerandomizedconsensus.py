3617400a7c0915920384d15ff273aa4c8a805d9c
Nikola Koevski <koevskin@gmail.com>
2017-08-26 19:19:14 +0200

    Update Byzantine Randomized Consensus protocol class

---
+++ core/byzantinerandomizedconsensus.py
@@ -0,0 +1,13 @@
+from base.consensus import Consensus
+
+
+class ByzantineRandomizedConsensus(Consensus):
+    """
+    Implements a Byzantine Fault Tolerant Randomized Consensus Broadcast protocol.
+    """
+
+    def propose(self, message):
+        pass
+
+    def decide(self):
+        pass
