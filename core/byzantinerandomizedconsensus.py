6e8601e3f254f0c100a0497e502568cff05cd47d
Nikola Koevski <koevskin@gmail.com>
2017-08-27 13:37:36 +0200

    Update file permissions

--- core/byzantinerandomizedconsensus.py
+++ core/byzantinerandomizedconsensus.py
@@ -1,10 +1,18 @@
 from base.consensus import Consensus
+import queue
 
 
 class ByzantineRandomizedConsensus(Consensus):
     """
     Implements a Byzantine Fault Tolerant Randomized Consensus Broadcast protocol.
     """
+
+    def __init__(self):
+        self.message_queue = queue.Queue()
+        self.round = 0
+        self.phase = 0
+        self.message_values = dict()
+        
 
     def propose(self, message):
         pass
